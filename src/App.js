0c6f8d15d14b71665d731241a1a3baa1fb577713
Peter Pavlatos <pavlatos.peter@gmail.com>
2019-07-23 07:55:17 -0400

    React Leaflet files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MapWrapper from './Map'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MapWrapper />
+    </div>
+  );
+}
+
+export default App;
